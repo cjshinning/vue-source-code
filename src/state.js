9e81c5d791249e4161cba17a54b94b1c4b8b09b9
cjshinningg <784319417@qq.com>
2021-10-22 22:54:53 +0800

    3.解决代理问题

--- src/state.js
+++ src/state.js
@@ -8,10 +8,27 @@ export function initState(vm) { //状态初始化
   }
 }
 
+function proxy(vm, source, key) {
+  Object.defineProperty(vm, key, {
+    get() {
+      return vm[source][key];
+    },
+    set(newValue) {
+      vm[source][key] = newValue;
+    }
+  })
+}
+
 function initData(vm) {
   let data = vm.$options.data;
+  // vue2中会将data中的所有数据 进行数据劫持 Object.defineProperty
+  // 这时vm和data没有任何关系，通过_data进行关联
   data = vm._data = isFunction(data) ? data.call(vm) : data;
 
-  // 这时vm和data没有任何关系，通过_data进行关联
+  // 用户去vm.xxx => vm._data.xxx
+  for (let key in data) {
+    proxy(vm, '_data', key);
+  }
+
   observe(data);
 }
